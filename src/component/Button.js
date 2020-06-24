885ee7a3bdfb08f6e8b9211f23d609727bd2984f
Greg Panciera <gregpanciera@gmail.com>
2020-06-24 14:08:00 -0500

    simplify to 8 buttons; add counter to button

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,16 +1,25 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import "./Button.css";
 
 export default class Button extends React.Component {
-  static propTypes = {
-    name: PropTypes.string,
-    orange: PropTypes.bool,
-    wide: PropTypes.bool,
-    clickHandler: PropTypes.func,
-  };
+  constructor(props){
+    super(props);
+    this.state = { clickCount: 0 };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  // static propTypes = {
+  //   name: PropTypes.string,
+  //   orange: PropTypes.bool,
+  //   wide: PropTypes.bool,
+  //   clickHandler: PropTypes.func,
+  // };
 
   handleClick = () => {
+    this.setState(state => {
+      return {clickCount: state.clickCount + 1}; 
+    })
     this.props.clickHandler(this.props.name);
   };
 
@@ -23,7 +32,9 @@ export default class Button extends React.Component {
 
     return (
       <div className={className.join(" ").trim()}>
-        <button onClick={this.handleClick}>{this.props.name}</button>
+        <button onClick={this.handleClick}>{this.props.name}
+          <div className='countText'>clicks:{this.state.clickCount}</div>
+        </button>
       </div>
     );
   }
