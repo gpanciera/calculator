352fc5599e32c70e50fa9d2edeacd32ea8a84f39
Greg Panciera <gregpanciera@gmail.com>
2020-07-14 21:59:46 -0500

    remove reactime npm hook

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./component/App";
 import "./index.css";
 import "github-fork-ribbon-css/gh-fork-ribbon.css";
-import reactime from 'dev-reactime';
+// import reactime from 'dev-reactime';
 
 const rootContainer = document.getElementById('root');
 ReactDOM.render(<App />, rootContainer);
-reactime(rootContainer);
+// reactime(rootContainer);
